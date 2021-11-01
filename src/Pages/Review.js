64a043a9b975851a7b93eb8f1ec6c6c95faada30
Keara Pereira <kearapereira@Kearas-MacBook-Pro.local>
2021-11-01 10:45:46 -0400

    review component wip

--- src/Pages/Review.js
+++ src/Pages/Review.js
@@ -7,33 +7,29 @@ const Review = ({ addReview }) => {
 
   function handleSubmit(event) {
     event.preventDefault();
-    addReview(review);
     setReview({ review: "" });
+    console.log(review)
   }
 
   function handleChange(event) {
-    setReview({ ...review, [event.target.name]: event.target.value });
+    setReview({ ...review, review: event.target.value });
   }
 
-  return(
-<form className = "reviewForm" onSubmit = {handleSubmit}>
-<label>Submit a Review</label>
-<textarea name="
-" id="review" cols="30" rows="10"></textarea>
-<input 
-className = "reviewForm"
-name = "review"
-value = {review.review}
-onChange = {handleChange}
-/>
-<button type = "submit">Add Review</button>
+  return (
+    <form className="reviewForm" onSubmit={handleSubmit}>
+      <label>Submit a Review</label>
+      <textarea
+        id="review"
+        cols="30"
+        rows="10"
+        className="reviewForm"
+        name="review"
+        value={review.review}
+        onChange={handleChange}
+      ></textarea>
 
-
-
-</form>
-
-
-
+      <button type="submit">Add Review</button>
+    </form>
   );
 };
 
